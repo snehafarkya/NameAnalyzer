e97a76cee2680a16f08ad40f3dc1f69c0240662f
snehafarkya <snhafarkya@gmail.com>
2023-10-07 20:17:24 +0530

    css updated

--- src/component/gender.js
+++ src/component/gender.js
@@ -37,16 +37,16 @@ export default function Gender() {
   //        });
   //  }, []);
   return (
-    <div className=' mx-auto w-[400px] my-20 bg-pink-500 p-4 rounded-xl border-4 border-double border-blue-100 shadow-2xl'>
-      
+    <div className=' mx-auto w-[400px] my-20 bgg px-4 pb-8 rounded-xl border-4 border-double border-blue-100 shadow-2xl'>
+      <h2 className='name pt-4 pb-4'>Name Analyzer</h2>
 
       <div >
       <div class="mb-6 ">
     <label for="first" class="block mb-2 text-sm font-semibold text-white">Firstname</label>
-    <input type="text" id="first" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-gray-400 dark:text-pink-700 dark:focus:border-pink-500 focus:outline-none" placeholder="Write your first name" required />
+    <input type="text" id="first" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-gray-400 dark:text-pink-700 dark:focus:border-pink-500 focus:outline-none font-medium hover:scale-[1.03] transition ease-in-out duration-300 hover:shadow-md" placeholder="Write your first name" required />
   </div>  <div class="mb-6">
     <label for="last" class="block mb-2 text-sm font-semibold text-white">Lastname</label>
-    <input type="text" id="last" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-pink-700 focus:outline-none dark:focus:border-pink-500" placeholder="Write your last name" required />
+    <input type="text" id="last" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-pink-700 focus:outline-none dark:focus:border-pink-500 font-medium" placeholder="Write your last name" required />
   </div>
   {/* <div class="mb-6">
     <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
