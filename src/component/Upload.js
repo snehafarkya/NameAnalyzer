d29ce11ed89884952319efd2b6b6bfc0e8e69352
snehafarkya <snhafarkya@gmail.com>
2023-10-08 19:33:35 +0530

    appwrite database added

--- src/component/Upload.js
+++ src/component/Upload.js
@@ -41,3 +41,4 @@ function Upload() {
 }
 
 export default Upload;
+
---
+++ src/component/appwrite.js
@@ -0,0 +1,20 @@
+import { Client, Databases, ID } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('6522b4c69f6fc61c4f5f');
+
+const databases = new Databases(client);
+
+const promise = databases.createDocument(
+    '6522b5ccaf2251170a66',
+    '6522b5eca4f169504281',
+    ID.unique(),
+    { "title": "Hamlet" }
+);
+
+promise.then(function (response) {
+    console.log(response);
+}, function (error) {
+    console.log(error);
+});
