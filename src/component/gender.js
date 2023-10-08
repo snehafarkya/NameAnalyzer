3e06d2e0c16b152fb2e3b007eca4b693e15a7857
snehafarkya <snhafarkya@gmail.com>
2023-10-08 17:33:11 +0530

    comments deleted

--- src/component/gender.js
+++ src/component/gender.js
@@ -37,7 +37,7 @@ export default function Gender() {
   //        });
   //  }, []);
   return (
-    <div className=' mx-auto w-[300px] md:w-[400px] my-20 bgg px-4 pb-8 rounded-xl border-4 border-double border-blue-100 hover:shadow-2xl  md:hover:scale-[1.2] hover:scale-[1.09] rotate-0 transition ease-in-out duration-1000'>
+    <div className=' mx-auto w-[300px] md:w-[400px] my-20 bgg px-4 pb-8 rounded-xl border-4 border-double border-blue-100 hover:shadow-2xl  md:hover:scale-[1.02] focus:scale-[1.07] rotate-0 transition ease-in-out duration-700'>
       <h2 className='name pt-4 pb-4 '>Name Analyzer</h2>
 
       <div >
@@ -48,20 +48,6 @@ export default function Gender() {
     <label for="last" class="block mb-2 text-sm font-semibold text-white">Lastname</label>
     <input type="text" id="last" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-pink-700 focus:outline-none dark:focus:border-pink-500 font-medium hover:scale-[1.03] transition ease-in-out duration-300 hover:shadow-md" placeholder="Write your last name" required />
   </div>
-  {/* <div class="mb-6">
-    <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-  </div> */}
-  {/* <div class="mb-6">
-    <label for="password" class="block mb-2 text-sm font-medium text-white">Your password</label>
-    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-  </div> */}
-  {/* <div class="flex items-start mb-6">
-    <div class="flex items-center h-5">
-      <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300  dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-    </div>
-    <label for="remember" class="ml-2 text-sm font-medium text-gray-300">Remember me</label>
-  </div> */}
   <button type="submit" class="text-pink-700 hover:bg-pink-300 hover:text-white font-bold bg-pink-200 focus:ring-4 focus:outline-none focus:ring-pink-400 rounded-md text-sm w-max sm:w-auto px-5 py-2.5 text-center hover:scale-[1.03] transition ease-in-out duration-200 hover:shadow-md " onClick={handleClick} >Identify here</button>
   {show ? (
     <>
