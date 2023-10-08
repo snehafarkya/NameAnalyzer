3f0d060878a837a6f9557a1c4e20f35e53bda561
snehafarkya <snhafarkya@gmail.com>
2023-10-08 18:04:14 +0530

    animations updated

--- src/component/gender.js
+++ src/component/gender.js
@@ -26,7 +26,7 @@ export default function Gender() {
   }
   return (
     <div className=' mx-auto w-[300px] md:w-[400px] my-20 bgg px-4 pb-8 rounded-xl border-4 border-double border-blue-100 hover:shadow-2xl  rotate-0 transition ease-in-out duration-700'>
-      <h2 className='name pt-4 pb-4 '>Name Analyzer</h2>
+      <h2 className='name pt-4 pb-4 '>Gender Analyzer</h2>
 
       <div >
       <div class="mb-6 ">
