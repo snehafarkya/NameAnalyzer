a1daa4ab20f8fc99c95fe32970ba87a9af091808
snehafarkya <snhafarkya@gmail.com>
2023-10-08 17:59:36 +0530

    animations updated

--- src/component/gender.js
+++ src/component/gender.js
@@ -36,7 +36,7 @@ export default function Gender() {
     <label for="last" class="block mb-2 text-sm font-semibold text-white">Lastname</label>
     <input type="text" id="last" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-pink-700 focus:outline-none dark:focus:border-pink-500 font-medium hover:scale-[1.03] transition ease-in-out duration-300 hover:shadow-md" placeholder="Write your last name" required />
   </div>
-  <button type="submit" class="text-pink-700 hover:bg-pink-300 hover:text-white font-bold bg-pink-200 focus:ring-4 focus:outline-none focus:ring-pink-400 rounded-md text-sm w-max sm:w-auto px-5 py-2.5 text-center hover:scale-[1.03] transition ease-in-out duration-200 hover:shadow-md " onClick={handleClick} >Identify here</button>
+  <button type="submit" class="text-pink-700 hover:bg-gradient-to-r from-blue-500  hover:text-blue-900 font-bold bg-pink-200 focus:ring-4 focus:outline-none focus:ring-pink-400 rounded-md text-sm w-max sm:w-auto px-5 py-2.5 text-center  transition ease-in-out duration-700 hover:shadow-md " onClick={handleClick} >Identify here</button>
   {show ? (
     <>
   <p className='text-white text-base my-4'>Your Gender is 💙💗: <span className='uppercase font-bold'>{posts}</span> </p>
